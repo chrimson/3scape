e62763c9d917c7efd6b05c454d5cfe1d3dcbce35
Chris Limson <chrimson@gmail.com>
2024-12-05 20:29:36 +0000

    Room

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer;
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+const scene = new THREE.Scene;
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.rotateX(-1/8 * Math.PI);
+camera.position.set(0, 1, 3);
 
+const geometry = new THREE.PlaneGeometry(1, 1, 10, 10);
+const material = new THREE.MeshStandardMaterial({color: 0xefefdf, side: THREE.DoubleSide});
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1, 5, 5, 5 );
-const material = new THREE.MeshStandardMaterial;
-const cube = new THREE.Mesh( geometry, material );
-cube.rotateX(1/4 * Math.PI);
-cube.rotateY(1/4 * Math.PI);
-scene.add( cube );
+const planeN = new THREE.Mesh(geometry, material);
+planeN.translateZ(-0.5);
 
-const light = new THREE.AmbientLight( 0xfff, 0.05 );
-scene.add( light );
+const planeE = new THREE.Mesh(geometry, material);
+planeE.translateX(0.5);
+planeE.rotateY(0.5 * Math.PI);
 
-const light1 = new THREE.PointLight( 0x00f, 10000 );
-scene.add( light1 );
+const planeW = new THREE.Mesh(geometry, material);
+planeW.translateX(-0.5);
+planeW.rotateY(0.5 * Math.PI);
 
-camera.position.set(0, 0, 5);
-light1.position.z = 5;
-light1.position.x = 2;
-light1.position.y = -1;
+const group = new THREE.Group;
+group.add(planeN);
+group.add(planeE);
+group.add(planeW);
+group.rotateY(1/8 * Math.PI);
+
+scene.add(group);
+
+const lightA = new THREE.AmbientLight(0x0f0f0e, 10);
+scene.add(lightA);
+const lightP = new THREE.PointLight(0x0f0f0e, 100);
+scene.add(lightP);
 
 function animate() {
-
-	const time = Date.now() * 0.0005;
-
-	renderer.render( scene, camera );
-
+  renderer.render(scene, camera);
 }
